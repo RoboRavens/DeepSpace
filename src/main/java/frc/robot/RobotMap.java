3a62fff62ca3ccc4d72e0995dad9280d279b5add
Alex Hoyer <ahoyer@wideopenwest.com>
2019-02-15 20:14:52 -0500

    modify robot map for new robot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,44 +8,36 @@ public class RobotMap {
 	public static int rightDriveChannel = 1;
 	public static int rightFollower1 = 12;
 	public static int rightFollower2 = 13;
-
-	// Drive encoders
-	public static final int leftDriveEncoder1 = 0;
-	public static final int leftDriveEncoder2 = 1;
-	public static final int rightDriveEncoder1 = 2;
-	public static final int rightDriveEncoder2 = 3;
-	
-	//public static final int shiftToLowGearSolenoid = 0;
-	//public static final int shiftToHighGearSolenoid = 1;
-	
-	//Relays 
-	public static final int hasCargoLEDLightRelay = 0;
-	public static final int hasHatchPanelLEDLightRelay = 2;
-	public static final int underglowLightRelay = 1;
 	
 	// Elevator System
 	public static final int elevatorMotor = 2;
+	public static final int elevatorMotorFollower = 3;
 	
-	public static final int elevatorEncoder1 = 20;
-	public static final int elevatorEncoder2 = 21;
-	
-	public static final int elevatorExtensionLimitSwitch = 7;
-	public static final int elevatorRetractionLimitSwitch = 6;
-	
-	// Camera
-	public static final String cameraName = "cam0";
-	
-	// Cargo System 
-	public static final int cargoMotor = 5;
-	public static final int cargoSensor = 5;
+	public static final int elevatorExtensionLimitSwitch = 0;
+	public static final int elevatorRetractionLimitSwitch = 1;
 
-	// Hatch Panel System
-	public static final int beakSolenoid = 2;
-	public static final int hatchPanelSensor = 6;
-	
 	// Arm System
 	public static final int armMotor = 4;
 
-	public static int armExtensionLimitSwitch = 8;
-	public static int armelevatorRetractionLimitSwitch = 9;
+	public static int armExtensionLimitSwitch = 2;
+	public static int armRetractionLimitSwitch = 3;
+	
+	// Cargo System 
+	public static final int cargoMotor = 5;
+	public static final int cargoSensor = 4;
+
+	// Hatch Panel System
+	public static final int beakSolenoid = 0;
+	public static final int hatchPanelSensor = 5;
+
+	//Climber
+	public static final int climberMotor = 6;
+
+	//Relays 
+	public static final int hasCargoLEDLightRelay = 0;
+	public static final int hasHatchPanelLEDLightRelay = 1;
+	public static final int underglowLightRelay = 2;
+
+	// Camera
+	public static final String cameraName = "cam0";
 }
