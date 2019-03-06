b8d6e01c50b51dc82855c67f69cf6c7567ef03d3
czbeatty <cameronzbeatty@gmail.com>
2019-03-06 18:57:22 -0500

    Adding linealignment commands folder and LineAlignmentCheck command

---
+++ src/main/java/frc/robot/commands/linealignment/LineAlignmentCheckCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.linealignment;
+
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LineAlignmentCheckCommand extends Command {
+
+    public LineAlignmentCheckCommand() {
+        requires(Robot.LINE_ALIGNMENT_SUBSYSTEM);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if (Robot.LINE_ALIGNMENT_SUBSYSTEM.getIsAligned()) {
+          Robot.LINE_ALIGNMENT_SUBSYSTEM.turnOn();
+        }
+        else {
+          Robot.LINE_ALIGNMENT_SUBSYSTEM.turnOff();
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
